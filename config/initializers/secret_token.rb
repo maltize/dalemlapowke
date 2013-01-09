f6baf74a8438622c630f12c705b619bfbd47da3b
Maciej Gajek <maltize@gmail.com>
2013-01-09 23:48:31 +0100

    empty version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dalemlapowke::Application.config.secret_token = '4dc48fbe6aadf734bacb0a7bb2904be654081312b4d5ac2b63622719d708482a60741b8fb73410ffde02c9d6c2cb3a2c8f55a642e89d1ca37eb232aa891c175b'
