aa283db73c1d058f30b13439c71fcde3807f0b5f
Maciej Gajek <maltize@gmail.com>
2013-01-16 20:27:24 +0100

    4 tabs on home - 8 of each kind of bribes story

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @bribes1 = Bribe.valid.where(:kind => 1).limit(8)
+    @bribes2 = Bribe.valid.where(:kind => 2).limit(8)
+    @bribes3 = Bribe.valid.where(:kind => 3).limit(8)
+    @bribes4 = Bribe.valid.where(:kind => 4).limit(8)
   end
 end
