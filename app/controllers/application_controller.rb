703b0aba6518861b85ad21994472e540b7ddb982
Maciej Gajek <maltize@gmail.com>
2013-01-26 11:18:48 +0100

    pass for production for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+private
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "admin" && password == "dalemlapowke.123"
+      end
+    end
+  end
+
 end
